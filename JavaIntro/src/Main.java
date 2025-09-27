41e9f2ada0b35ee6b488fd7e7a597f4013fcf937
radoykov <alex.radoykov@gmail.com>
2025-09-27 15:47:31 +0300

    Intro in java created.

---
+++ JavaIntro/src/Main.java
@@ -0,0 +1,16 @@
+import java.net.SocketOption;
+
+public class Main {
+    public static void main(String[] args) {
+        Vehicle car = new Vehicle();
+        car.displayInfo();
+        Vehicle car2 = new Vehicle("white");
+        //  car = null// tearing the link and garbage collector will delete the obj
+
+        System.out.println(car.getColor());
+        System.out.println(car2.getColor());
+
+        car.displayInfo();
+        car2.displayInfo();
+    }
+}
---
+++ JavaIntro/src/Vehicle.java
@@ -0,0 +1,28 @@
+class Vehicle {
+    private final int id;           //created in stack
+    private static int counter = 0; //created in stack
+    private String color;          //created in stack
+
+    public Vehicle() {   //default constructor
+        counter++;
+        id = counter;
+        color = "brown";
+    }
+
+    public Vehicle(String color) {
+        this();
+        setColor(color);
+    }
+
+    private void setColor(String color) {
+        this.color = color;
+    }
+
+    public String getColor() {
+        return this.color;
+    }
+
+    public void displayInfo() {
+        System.out.println("Count : " + counter + " " + "ID : " + id + " " + "Colour : " + color);
+    }
+}
